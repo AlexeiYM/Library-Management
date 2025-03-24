9fec97c77bc13641916ba4527eb7af472c229edc
Алексей <alyakushevich.2006@gmail.com>
2025-03-24 13:59:27 +0300

    pre commit

--- app/src/main/java/com/example/library/reified.kt
+++
@@ -1,9 +0,0 @@
-package com.example.library
-
-inline fun <reified T> getNewList(sourceList: List<*>): MutableList<T> {
-    val newList = mutableListOf<T>()
-    sourceList.forEach {
-        if (it is T) newList.add(it)
-    }
-    return newList
-}
